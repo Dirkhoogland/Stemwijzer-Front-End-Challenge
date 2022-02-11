2867e667f72f873db2e25e496b8b094d2b49b578
Dirkhoogland <99063413@mydavinci.nl>
2022-02-11 09:04:53 +0100

    antwoorden array aangemaakt

--- Stemwijzer-Front-End-Challenge/Pages/Index.cshtml.cs
+++ Stemwijzer-Front-End-Challenge/Pages/Index.cshtml.cs
@@ -8,15 +8,48 @@ using System.Threading.Tasks;
 
 namespace Stemwijzer_Front_End_Challenge.Pages
 {
-    
+   
 
 
     public class IndexModel : PageModel
-    { 
+    {
+
+        string[] antwoorden = new string[] {"-","-","-","-" }; 
         
         public void OnGet()
         {
             
+        }
+        // Testen op correcte match, voer in:
+        // PVV: contra, pro, contra, pro
+        // SP: pro, contra, contra, contra
+        // D66: pro, contra, pro, contra
+        // CU: pro, pro, pro, contra
+
+        
+
+        public void Bindend_referendum()
+        {
+            
+
+        }
+
+        public void Maatschappelijke_dienstplicht()
+        {
+
+
+        }
+
+        public void Anoniem_solliciteren()
+        {
+
+
+        }
+
+        public void Groepsbelediging()
+        {
+
+
         }
 
     }
--- Stemwijzer-Front-End-Challenge/wwwroot/js/site.js
+++ Stemwijzer-Front-End-Challenge/wwwroot/js/site.js
@@ -6,34 +6,36 @@ var Geenmening = document.getElementById("Geenmening");
 var ButtonsDiv = document.getElementById("ButtonsDiv");
 var Vraag = document.getElementById("Vraag");
 var Statement = document.getElementById("Statement");
+var NummerVraag = document.getElementById("Uitleg");
+
 // functie voor de eens oneens en geen menng knoppen voor te laten komen en voor de eerste vraag en statement
 function AlterButtons()
 {
-    
     ButtonsDiv.style.visibility = "visible"
     ButtonStart.style.visibility = "hidden"
     Vraag.innerHTML = "Er moet een bindend referendum komen, waarmee burgers door het parlement aangenomen wetten kunnen tegenhouden."
     Statement.innerHTML = "Bindend referendum"
-
+    NummerVraag.innerHTML = "Vraag 1"
 };
 // functie voor de tweede vraag en statement
 function vraag2()
 {
     Vraag.innerHTML = "Er moet een maatschappelijke dienstplicht voor jongeren komen. Zij kunnen dan dienen in het leger, bij de politie of in de zorg."
     Statement.innerHTML = "Maatschappelijke dienstplicht"
+    NummerVraag.innerHTML = "Vraag 2"
 }
 //functie voor de derde vraag en statement
 function vraag3()
 {
-
     Vraag.innerHTML = "Om discriminatie op basis van de naam te voorkomen, moet anoniem solliciteren bij de overheid en bij openbare instellingen de regel worden."
     Statement.innerHTML = "Anoniem solliciteren"
+    NummerVraag.innerHTML = "Vraag 3"
 }
 //functie voor de vierde vraag en statement
 function vraag4()
 {
-
     Vraag.innerHTML = "Belediging van groepen op grond van ras, godsdienst of geaardheid moet niet langer strafbaar zijn."
     Statement.innerHTML = "Groepsbelediging"
+    NummerVraag.innerHTML = "Vraag 4"
 }
 
